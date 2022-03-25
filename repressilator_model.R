22db0e78ef30a14ac18b6e45511294e9de177cff
WangM <74388983+stayn1ght@users.noreply.github.com>
2022-03-26 01:13:48 +0800

    Update repressilator_model.R

--- repressilator_model.R
+++ repressilator_model.R
@@ -1,5 +1,7 @@
-library(deSolve)
-library(reshape2)
+library(deSolve) # to solve the differencial equation
+library(ggplot2) # to draw a plot of the solution
+library(reshape2) # to draw
+
 repressilator_model <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     dA <-  -A + a/(1 + Z^n) + a0
@@ -13,10 +15,18 @@ repressilator_model <- function(t, state, parameters) {
 }
 
 parameters <- c(a0 = 0.5, a = 500, b = 5, n = 2)
-state      <- c(A = 10, B = 9, C = 8, X = 0, Y = 0, Z = 0) 
-#in this model the initial of A, B and C, which reffers to mRNA, should not set as the same 
-#if a synthetic oscilatory is expected.
-times      <- seq(0, 100, by = 0.1)
+state      <- c(A = 10, B = 9, C = 8, X = 0, Y = 0, Z = 0)
+times      <- seq(0, 50, by = 0.1)
 
 out <- ode(y = state, times = times, 
            func = repressilator_model, parms = parameters)
+
+
+out <- data.frame(out)
+protein <- cbind(out[1], out[5:7])
+protein <- melt(protein, id.vars = 'time')
+
+ggplot(data = protein, aes(time, value, group = variable, 
+                        color = variable)) +
+  geom_line() +
+  labs(x="time",y="Protein")
