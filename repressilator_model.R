1c40f896d2f0b0adfb2e069aa9b7f15ea3d26190
WangM <74388983+stayn1ght@users.noreply.github.com>
2022-03-26 00:27:28 +0800

    ok

--- repressilator_model.R
+++ repressilator_model.R
@@ -2,19 +2,21 @@ library(deSolve)
 library(reshape2)
 repressilator_model <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
-    dm1 <-  -m1 + a/(1 + p3^2) + a0
-    dp1 <-  -b * (p1 - m1)
-    dm2 <-  -m2 + a/(1 + p1^2) + a0
-    dp2 <-  -b * (p2 - m2)
-    dm3 <-  -m3 + a/(1 + p2^2) + a0
-    dp3 <-  -b * (p3 - m3)
-    list(c(dm1, dp1,dm2,dp2,dm3,dp3))
+    dA <-  -A + a/(1 + Z^n) + a0
+    dX <-  -b * (X - A)
+    dB <-  -B + a/(1 + X^n) + a0
+    dY <-  -b * (Y - B)
+    dC <-  -C + a/(1 + Y^n) + a0
+    dZ <-  -b * (Z - C)
+    list(c(dA, dB, dC, dX, dY, dZ))
   })
 }
 
-parameters <- c(a0 = 0.0005, a = 0.5, b = 5)
-state      <- c(m1 = 10, m2 = 10, m3 = 10, p1 = 0, p2 = 0, p3 = 0)
-times      <- seq(0, 100, by = 1)
+parameters <- c(a0 = 0.5, a = 500, b = 5, n = 2)
+state      <- c(A = 10, B = 9, C = 8, X = 0, Y = 0, Z = 0) 
+#in this model the initial of A, B and C, which reffers to mRNA, should not set as the same 
+#if a synthetic oscilatory is expected.
+times      <- seq(0, 100, by = 0.1)
 
 out <- ode(y = state, times = times, 
            func = repressilator_model, parms = parameters)
